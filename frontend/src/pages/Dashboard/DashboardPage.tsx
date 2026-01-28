e46db031b16f5385f70f8476e92b136367118699
Cal <callumburgoyne04@gmail.com>
2026-01-28 23:38:50 +0000

    feat: add empty dashboard page component

---
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "../../layout/Sidebar/Sidebar";
+
+const DashboardPage = () => {
+    return (
+        <>
+            <Sidebar />
+            <main className="lg:ml-sidebar-width">DashboardPage</main>
+        </>
+    );
+};
+
+export default DashboardPage;
