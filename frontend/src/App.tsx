9c4558697b64f085efcbd6d37bde88f61fc68cf2
Cal <callumburgoyne04@gmail.com>
2026-01-28 23:38:57 +0000

    feat: add dashboard page to router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/Home/HomePage";
 import NotFoundPage from "./pages/NotFound/NotFoundPage";
+import Navbar from "./layout/Navbar/Navbar";
+import DashboardPage from "./pages/Dashboard/DashboardPage";
 
 function App() {
     return (
         <BrowserRouter>
+            <Navbar />
             <Routes>
+                {/* Landing Page */}
                 <Route path="/" element={<HomePage />} />
+                {/* Application Pages */}
+                <Route path="/dashboard" element={<DashboardPage />} />
                 {/* Not found page - all un-routed paths */}
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
