1a2700283829fec3c15a9b59eca288ba5978a6d1
Cal <callumburgoyne04@gmail.com>
2026-01-30 10:38:41 +0000

    feat: add CORS config

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,17 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/CallumB04/ticket-system/backend/internal/api"
+	"github.com/joho/godotenv"
+	"github.com/rs/cors"
 )
 
 func main() {
 	// Create multiplexer and register router handlers.
 	mux := api.RegisterHandlers()
 
+	// Load environment variables file
+	if err := godotenv.Load(); err != nil {
+		log.Println("Failed to load .env file")
+	}
+
+	// Get frontendURLs from environment.
+	// If empty, default to localhost with common Vite port.
+	frontendURLs := strings.Split(os.Getenv("FRONTEND_URLS"), ",")
+	if len(frontendURLs) == 0 {
+		frontendURLs = []string{"http://localhost:5173"}
+	}
+
+	// Configure CORS
+	c := cors.New(cors.Options{
+		AllowedOrigins:   frontendURLs,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
+		AllowedHeaders:   []string{"Content-Type"},
+		AllowCredentials: true,
+	})
+
+	// Apply CORS specification to requests
+	handler := c.Handler(mux)
+
 	// Start server
-	fmt.Println("Server is online")
-	http.ListenAndServe(":8080", mux)
+	log.Println("Server is online")
+	http.ListenAndServe(":8080", handler)
 }
