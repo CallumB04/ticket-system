7cb1fb4b8923600167caac87218fa183b2521841
Cal <callumburgoyne04@gmail.com>
2026-02-02 01:04:06 +0000

    chore: add new elements for testing new api calls

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import {
     createOrganisation,
+    fetchOrganisationMembers,
     fetchOrganisations,
+    type OrganisationMember,
     type Organisation,
 } from "../../api";
 import { signIn, signUp } from "../../supabase/users";
@@ -9,6 +11,7 @@ import { useUser } from "../../hooks/useUser";
 
 const HomePage = () => {
     const [orgs, setOrgs] = useState<Organisation[]>([]);
+    const [members, setMembers] = useState<OrganisationMember[]>([]);
 
     const { sessionLoading, user, signOut } = useUser();
 
@@ -24,6 +27,14 @@ const HomePage = () => {
         const resp = await fetchOrganisations();
         if (resp) {
             setOrgs(resp);
+        }
+    };
+
+    const fetchMembers = async () => {
+        const resp = await fetchOrganisationMembers(orgs[0].id);
+        if (resp) {
+            setMembers(resp);
+            console.log(resp);
         }
     };
 
@@ -67,8 +78,27 @@ const HomePage = () => {
                 >
                     Fetch orgs
                 </button>
+                <button
+                    onClick={fetchMembers}
+                    className="bg-btn-secondary hover:bg-btn-secondary-hover text-btn-secondary-text border-btn-secondary-border hover:border-btn-secondary-hover-border h-12 w-50 cursor-pointer rounded border-2"
+                >
+                    Fetch members
+                </button>
             </span>
             <p>{orgs.length === 0 ? "No orgs" : orgs.map((o) => o.name)}</p>
+            <p>
+                {members.length === 0
+                    ? "No members"
+                    : members.map((m) => (
+                          <span>
+                              {m.user.first_name
+                                  ? m.user.first_name
+                                  : "unknown name"}
+                              {": "}
+                              {m.role}
+                          </span>
+                      ))}
+            </p>
             <p>{sessionLoading ? "loading email..." : user?.email}</p>
             <div className="bg-surface mt-2 size-20 rounded"></div>
             <div className="bg-surface-muted mt-2 size-20 rounded"></div>
