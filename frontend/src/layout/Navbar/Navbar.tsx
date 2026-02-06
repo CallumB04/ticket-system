6cf80d0f97636a49e91153a5eca8dccb2828d8ed
CallumB04 <callumburgoyne04@gmail.com>
2026-02-06 00:29:00 +0000

    feat: add max width to navbar content

--- frontend/src/layout/Navbar/Navbar.tsx
+++ frontend/src/layout/Navbar/Navbar.tsx
@@ -17,58 +17,64 @@ const Navbar = ({ className }: NavbarProps) => {
     return (
         <nav
             className={twMerge(
-                "bg-surface border-b-layout-border h-navbar-height fixed top-0 left-0 flex w-screen items-center justify-between border-b px-6",
+                "bg-surface border-b-layout-border h-navbar-height fixed top-0 left-0 flex w-screen items-center border-b px-4 sm:px-6",
                 className
             )}
         >
-            {/* Logo / Brand text */}
-            <Link
-                to="/"
-                className="text-text-primary font-medium tracking-wide"
-            >
-                Ticket System
-            </Link>
-            {/* Buttons */}
-            {/* When signed out -> Signup / Login */}
-            {/* When signed in -> Go to Dashboard */}
-            {sessionLoading ? (
-                <></>
-            ) : user ? (
-                <LinkButton to="/dashboard" variant="primary" className="h-11">
-                    Go to Dashboard
-                </LinkButton>
-            ) : (
-                <span className="flex gap-2">
-                    <Button
+            <span className="mx-auto flex w-full max-w-5xl items-center justify-between gap-4">
+                {/* Logo / Brand text */}
+                <Link
+                    to="/"
+                    className="text-text-primary font-medium tracking-wide"
+                >
+                    Ticket System
+                </Link>
+                {/* Buttons */}
+                {/* When signed out -> Signup / Login */}
+                {/* When signed in -> Go to Dashboard */}
+                {sessionLoading ? (
+                    <></>
+                ) : user ? (
+                    <LinkButton
+                        to="/dashboard"
                         variant="primary"
-                        className="h-11 w-22"
-                        onClick={() =>
-                            pushPopup(
-                                <SignupLoginPopup
-                                    closePopup={popPopup}
-                                    initialState="signup"
-                                />
-                            )
-                        }
+                        className="h-11"
                     >
-                        Sign up
-                    </Button>
-                    <Button
-                        variant="secondary"
-                        className="h-11 w-22"
-                        onClick={() =>
-                            pushPopup(
-                                <SignupLoginPopup
-                                    closePopup={popPopup}
-                                    initialState="login"
-                                />
-                            )
-                        }
-                    >
-                        Log in
-                    </Button>
-                </span>
-            )}
+                        Go to Dashboard
+                    </LinkButton>
+                ) : (
+                    <span className="flex gap-2">
+                        <Button
+                            variant="primary"
+                            className="h-11 w-22"
+                            onClick={() =>
+                                pushPopup(
+                                    <SignupLoginPopup
+                                        closePopup={popPopup}
+                                        initialState="signup"
+                                    />
+                                )
+                            }
+                        >
+                            Sign up
+                        </Button>
+                        <Button
+                            variant="secondary"
+                            className="h-11 w-22"
+                            onClick={() =>
+                                pushPopup(
+                                    <SignupLoginPopup
+                                        closePopup={popPopup}
+                                        initialState="login"
+                                    />
+                                )
+                            }
+                        >
+                            Log in
+                        </Button>
+                    </span>
+                )}
+            </span>
         </nav>
     );
 };
