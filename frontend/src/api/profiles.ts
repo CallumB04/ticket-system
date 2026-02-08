0900d390461e8ded920a5d4b618783247e1c5e58
Cal <callumburgoyne04@gmail.com>
2026-02-08 11:04:38 +0000

    feat: add api function to fetch user profile

--- frontend/src/api/profiles.ts
+++ frontend/src/api/profiles.ts
@@ -1,5 +1,7 @@
 // Models
 
+import { api } from ".";
+
 export type UserProfile = {
     id: string;
     first_name: string | null;
@@ -7,3 +9,11 @@ export type UserProfile = {
     avatar_url: string | null;
     created_at: string;
 };
+
+// API Calls
+
+// Fetches user profile of the signed in user
+export async function fetchUserProfile() {
+    const res = await api.get<UserProfile>("/v1/profile");
+    return res.data;
+}
