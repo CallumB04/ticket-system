0cb36907bbd05a27ade7c49352c37879d9161502
Cal <callumburgoyne04@gmail.com>
2026-02-24 01:22:23 +0000

    feat: add dropdown component

---
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,58 @@
+import { type ChangeEvent } from "react";
+import { twMerge } from "tailwind-merge";
+
+export type DropdownOption = {
+    label: string;
+    value: string;
+};
+
+interface DropdownProps {
+    className?: string;
+    containerClassName?: string; // div containing label and dropdown
+    options: DropdownOption[];
+    label?: string;
+    defaultOption?: string;
+    disabled?: boolean;
+    onChange?: (value: string) => void;
+}
+
+const Dropdown = ({
+    className,
+    containerClassName,
+    options,
+    label,
+    defaultOption,
+    disabled,
+    onChange,
+}: DropdownProps) => {
+    const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+        // send new value to parent component
+        if (onChange) {
+            onChange(event.target.value);
+        }
+    };
+
+    return (
+        <div className={twMerge("space-y-1", containerClassName)}>
+            {label && (
+                <p className="text-text-primary text-xs font-semibold">
+                    {label}
+                </p>
+            )}
+            <select
+                defaultValue={defaultOption}
+                disabled={disabled}
+                onChange={handleChange}
+                className={twMerge("input-default", className)}
+            >
+                {options.map((o) => (
+                    <option key={o.value} value={o.value}>
+                        {o.label}
+                    </option>
+                ))}
+            </select>
+        </div>
+    );
+};
+
+export default Dropdown;
