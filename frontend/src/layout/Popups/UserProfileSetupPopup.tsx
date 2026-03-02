dd5f69b6f77ea67c924a82805b2dc7ca1b6a8c5b
Cal <callumburgoyne04@gmail.com>
2026-03-02 02:21:35 +0000

    feat: update user profile when saved

--- frontend/src/layout/Popups/UserProfileSetupPopup.tsx
+++ frontend/src/layout/Popups/UserProfileSetupPopup.tsx
@@ -5,6 +5,7 @@ import Input from "../../components/Input/Input";
 import Popup from "../../components/Popup/Popup";
 import PopupButtonsContainer from "../../components/Popup/PopupButtonsContainer";
 import { ALL_COUNTRIES } from "../../constants/countries";
+import { updateUserProfile } from "../../api/profiles";
 
 interface UserProfileSetupPopupProps {
     closePopup: () => void;
@@ -15,6 +16,30 @@ const UserProfileSetupPopup = ({ closePopup }: UserProfileSetupPopupProps) => {
     const [firstName, setFirstName] = useState<string>("");
     const [lastName, setLastName] = useState<string>("");
     const [country, setCountry] = useState<string>("");
+
+    // is user profile update request currently loading
+    const [isSaving, setIsSaving] = useState<boolean>(false);
+
+    const handleSave = async () => {
+        try {
+            setIsSaving(true);
+
+            const newProfile = await updateUserProfile({
+                first_name: firstName,
+                last_name: lastName,
+                country: country,
+            });
+
+            if (newProfile.first_name) {
+                closePopup();
+            }
+        } catch {
+            // TODO: add proper error messaging on popup
+            alert("Error updating profile, please try again");
+        } finally {
+            setIsSaving(false);
+        }
+    };
 
     return (
         <Popup
@@ -49,10 +74,10 @@ const UserProfileSetupPopup = ({ closePopup }: UserProfileSetupPopupProps) => {
                 <Button
                     variant="primary"
                     className="w-full"
-                    disabled={firstName.length < 3}
-                    onClick={closePopup}
+                    disabled={firstName.length < 3 || isSaving}
+                    onClick={handleSave}
                 >
-                    Save
+                    {isSaving ? "Saving..." : "Save"}
                 </Button>
             </PopupButtonsContainer>
         </Popup>
