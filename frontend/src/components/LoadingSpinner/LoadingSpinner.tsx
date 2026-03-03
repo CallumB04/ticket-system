72a28969a1d3130e145bd428d853af3bfdbe643d
Cal <callumburgoyne04@gmail.com>
2026-03-03 10:54:36 +0000

    feat: add loading spinner component

---
+++ frontend/src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,31 @@
+import { twMerge } from "tailwind-merge";
+
+interface LoadingSpinnerProps {
+    className?: string;
+    variant: "bg" | "surface" | "btn-disabled"; // determines spinner and center colors
+}
+
+const LoadingSpinner = ({ className, variant }: LoadingSpinnerProps) => {
+    return (
+        <div
+            className={twMerge(
+                "to-highlight animate-spin rounded-full bg-conic from-transparent",
+                variant === "btn-disabled"
+                    ? "to-btn-primary-disabled-text size-4 p-px"
+                    : "to-highlight size-8 p-0.5",
+                className
+            )}
+        >
+            <div
+                className={twMerge(
+                    "size-full rounded-full",
+                    variant === "bg" && "bg-background",
+                    variant === "surface" && "bg-surface",
+                    variant === "btn-disabled" && "bg-btn-primary-disabled"
+                )}
+            ></div>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
