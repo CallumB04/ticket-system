a29d3ee8b86b6e4b108cc2a8c7f6a52d9fdb32c7
Cal <callumburgoyne04@gmail.com>
2026-03-04 00:27:53 +0000

    feat: add page component

---
+++ frontend/src/components/Page/Page.tsx
@@ -0,0 +1,42 @@
+import type { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+import PageHeader from "./PageHeader";
+import PageDescription from "./PageDescription";
+
+interface PageProps {
+    className?: string;
+    children?: ReactNode;
+    title?: string;
+    description?: string;
+    hasSidebar?: boolean;
+}
+
+const Page = ({
+    className,
+    children,
+    title,
+    description,
+    hasSidebar,
+}: PageProps) => {
+    return (
+        <main
+            className={twMerge(
+                "mt-navbar-height flex min-h-[calc(100vh-var(--navbar-height))] justify-center p-4 sm:p-8",
+                hasSidebar && "lg:ml-sidebar-width",
+                className
+            )}
+        >
+            <div className={twMerge("w-full max-w-7xl space-y-8")}>
+                <div className="space-y-2">
+                    {title && <PageHeader>{title}</PageHeader>}
+                    {description && (
+                        <PageDescription>{description}</PageDescription>
+                    )}
+                </div>
+                {children}
+            </div>
+        </main>
+    );
+};
+
+export default Page;
