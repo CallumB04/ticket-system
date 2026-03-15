2f338fb1ca88681c2f0e302d45546951c925a578
Cal <callumburgoyne04@gmail.com>
2026-03-15 21:17:16 +0000

    feat: add sidebar provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeProvider } from "./contexts/ThemeContext.tsx";
 import { UserProvider } from "./contexts/UserContext.tsx";
 import { PopupProvider } from "./contexts/PopupContext.tsx";
+import SidebarProvider from "./contexts/SidebarContext.tsx";
 
 const queryClient = new QueryClient();
 
@@ -14,7 +15,9 @@ createRoot(document.getElementById("root")!).render(
             <PopupProvider>
                 <UserProvider>
                     <ThemeProvider>
-                        <App />
+                        <SidebarProvider>
+                            <App />
+                        </SidebarProvider>
                     </ThemeProvider>
                 </UserProvider>
             </PopupProvider>
