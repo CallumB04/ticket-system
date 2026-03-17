a26c7dd872bab8db0c858c549b94559fb75a4a58
Cal <callumburgoyne04@gmail.com>
2026-03-17 20:11:47 +0000

    feat: add function for fetching notifications

---
+++ frontend/src/api/notifications.ts
@@ -0,0 +1,21 @@
+// Models
+
+import { api } from ".";
+
+export type NotificationType = "welcome" | "org-invite"; // TODO: add more types
+
+export type Notification = {
+    id: string;
+    type: NotificationType;
+    description: string;
+    read: boolean;
+    created_at: string; // ISO string
+};
+
+// API Calls
+
+// Fetches all notifications belonging to the signed in user
+export const fetchNotifications = async () => {
+    const res = await api.get<Notification[]>("/v1/notifications");
+    return res.data;
+};
