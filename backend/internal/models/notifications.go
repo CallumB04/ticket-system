dfe4cec2f18a83fbd703e40af651be30c86d3bfb
Cal <callumburgoyne04@gmail.com>
2026-03-17 01:29:08 +0000

    feat: create notifications data model

---
+++ backend/internal/models/notifications.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Notification struct {
+	ID          string    `json:"id"`
+	Type        string    `json:"type"` // "welcome", "org-invite", etc
+	Description string    `json:"description"`
+	Read        bool      `json:"read"`
+	CreatedAt   time.Time `json:"created_at"`
+}
