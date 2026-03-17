4814fac6641e586157b085f24ade9bdbbbe037c1
Cal <callumburgoyne04@gmail.com>
2026-03-17 20:16:48 +0000

    feat: load and display notifications in popout

--- frontend/src/layout/Popouts/NotificationsPopout.tsx
+++ frontend/src/layout/Popouts/NotificationsPopout.tsx
@@ -1,49 +1,111 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import Popout from "../../components/Popout/Popout";
-import ClickableText from "../../components/Text/ClickableText";
-import { BellIcon } from "lucide-react";
+import { BellIcon, Building2Icon, SproutIcon } from "lucide-react";
 import useClickOutside from "../../hooks/useClickOutside";
 import { twMerge } from "tailwind-merge";
+import { useQuery } from "@tanstack/react-query";
+import { useUser } from "../../contexts/UserContext";
+import {
+    fetchNotifications,
+    type Notification,
+    type NotificationType,
+} from "../../api/notifications";
+import ClickableGroup from "../../components/ClickableGroup/ClickableGroup";
+import { getDaysAgoFromISO } from "../../util/date";
+import LoadingSpinner from "../../components/LoadingSpinner/LoadingSpinner";
 
 interface NotificationsPopoutProps {
     className?: string;
     closePopout: () => void;
 }
 
+const getIconFromNotificationType = (type: NotificationType) => {
+    switch (type) {
+        case "welcome":
+            return <SproutIcon size={18} className="text-highlight/50" />;
+        case "org-invite":
+            return <Building2Icon size={18} className="text-red-300" />;
+    }
+};
+
 const NotificationsPopout = ({
     className,
     closePopout,
 }: NotificationsPopoutProps) => {
-    const [notificationsShowAll, setNotificationsShowAll] =
-        useState<boolean>(false);
+    const { user } = useUser();
 
     const popoutRef = useRef<HTMLDivElement>(null);
     useClickOutside(popoutRef, closePopout); // close when click outside
+
+    // Load notifications on component mount
+    const {
+        data: notifications,
+        isLoading: notificationsLoading,
+        error: notificationsError,
+    } = useQuery({
+        queryKey: ["notifications", user?.id], // refetch when user changes
+        queryFn: async () => {
+            const notis = await fetchNotifications();
+            return notis ?? [];
+        },
+    });
+
+    // Notify user when notifications fetch errors
+    // TODO: Replace alert with Toast when Toaster is built
+    useEffect(() => {
+        if (notificationsError) {
+            alert("Notifications error");
+        }
+    }, [notificationsError]);
 
     return (
         <Popout
             xPos="left"
             yPos="bottom"
-            className={twMerge("flex h-76 w-72 flex-col p-0", className)}
+            className={twMerge(
+                "flex h-72 w-80 flex-col overflow-y-scroll",
+                className
+            )}
             ref={popoutRef}
             title="Notifications"
         >
-            {/* No Notifications */}
-            <div className="text-text-placeholder mt-16 flex w-full flex-col items-center gap-2.5">
-                <BellIcon size={32} />
-                <p className="text-sm">
-                    No {notificationsShowAll ? "" : "new"} notifications
-                </p>
-                {!notificationsShowAll && (
-                    <ClickableText
-                        className="text-xs"
-                        onClick={() => setNotificationsShowAll(true)}
-                    >
-                        View all
-                    </ClickableText>
-                )}
-            </div>
+            {notificationsLoading ? (
+                // Loading Notifications
+                <LoadingSpinner variant="surface" className="mx-auto mt-12" />
+            ) : notifications && notifications?.length >= 1 ? (
+                // Notifications
+                notifications.map((n) => (
+                    <NotificationPopoutItem notification={n} />
+                ))
+            ) : (
+                // No notifications
+                <div className="text-text-placeholder mt-12 flex w-full flex-col items-center gap-2.5">
+                    <BellIcon size={32} />
+                    <p className="text-sm">No notifications</p>
+                </div>
+            )}
         </Popout>
+    );
+};
+
+// Component for individual notifications
+const NotificationPopoutItem = ({
+    notification,
+}: {
+    notification: Notification;
+}) => {
+    return (
+        <ClickableGroup className="w-full items-start gap-3">
+            {getIconFromNotificationType(notification.type)}
+            <div className="flex-1 space-y-1">
+                <p className="text-text-primary text-sm">
+                    {notification.description}
+                </p>
+                <p className="text-text-secondary text-xs">
+                    {getDaysAgoFromISO(notification.created_at)}
+                </p>
+            </div>
+        </ClickableGroup>
     );
 };
 
