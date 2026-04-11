c2db0a492a931dcb69d64aefad596d6e1c831427
Cal <callumburgoyne04@gmail.com>
2026-04-11 02:28:02 +0100

    feat: add example statistics to dashboard

--- frontend/src/pages/Dashboard/DashboardPage.tsx
+++ frontend/src/pages/Dashboard/DashboardPage.tsx
@@ -1,4 +1,5 @@
 import Page from "../../components/Page/Page";
+import StatisticCard from "../../components/StatisticCard/StatisticCard";
 import usePageTitle from "../../hooks/usePageTitle";
 import Sidebar from "../../layout/Sidebar/Sidebar";
 
@@ -12,7 +13,31 @@ const DashboardPage = () => {
                 title="Dashboard"
                 description="Welcome to the home of booth"
                 hasSidebar
-            ></Page>
+            >
+                {/* Hero Analytics */}
+                <div className="flex w-full gap-8">
+                    <StatisticCard
+                        label="Active Tickets"
+                        value={14}
+                        className="w-full"
+                    />
+                    <StatisticCard
+                        label="My Tickets"
+                        value={5}
+                        className="w-full"
+                    />
+                    <StatisticCard
+                        label="Unassigned Tickets"
+                        value={3}
+                        className="w-full"
+                    />
+                    <StatisticCard
+                        label="Resolved Tickets"
+                        value={23}
+                        className="w-full"
+                    />
+                </div>
+            </Page>
         </>
     );
 };
