8b6165298fdb817935ed4eed733abaad0f746237
Cal <callumburgoyne04@gmail.com>
2026-04-14 14:07:55 +0100

    fix: prevent text highlights on navigator

--- frontend/src/components/Navigator/NavigatorItem.tsx
+++ frontend/src/components/Navigator/NavigatorItem.tsx
@@ -12,7 +12,7 @@ const NavigatorItem = ({ option, active, onClick }: NavigatorItemProps) => {
         <div
             onClick={onClick}
             className={twMerge(
-                "text-text-secondary hover:text-text-primary flex h-full cursor-default items-center rounded-xs px-4 font-mono text-sm transition-colors",
+                "text-text-secondary hover:text-text-primary flex h-full cursor-default items-center rounded-xs px-4 font-mono text-sm transition-colors select-none",
                 active &&
                     "text-surface hover:text-surface from-highlight to-highlight/60 bg-linear-45"
             )}
