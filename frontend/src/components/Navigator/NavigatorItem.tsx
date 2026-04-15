c47b773540035b3fd9086dee00b33ed57d6fee0f
Cal <callumburgoyne04@gmail.com>
2026-04-15 10:30:48 +0100

    feat: add inset shadow to navigator

--- frontend/src/components/Navigator/NavigatorItem.tsx
+++ frontend/src/components/Navigator/NavigatorItem.tsx
@@ -13,7 +13,8 @@ const NavigatorItem = ({ option, active, onClick }: NavigatorItemProps) => {
             onClick={onClick}
             className={twMerge(
                 "text-text-secondary hover:text-text-primary flex h-full cursor-default items-center rounded-xs px-4 font-mono text-sm transition-colors select-none",
-                active && "text-surface hover:text-surface bg-highlight"
+                active &&
+                    "text-surface hover:text-surface bg-highlight shadow-[inset_0_1px_3px_rgba(0,0,0,0.4)]"
             )}
         >
             {option.label}
